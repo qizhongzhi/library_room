cdf8ef790ddd497cd0a84c36d19bb0f231011c03
LeoJY <leojy1993@gmail.com>
2016-09-24 15:14:23 -0400

    finish the user module

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,24 @@
 class RegistrationsController < Devise::RegistrationsController
+
+	prepend_before_action :require_no_authentication, only: [:new]
+
+  def create
+    super #Nothing special here.
+  end
+
   protected
+
+  def sign_up(resource_name, resource)
+    true
+  end
+
   def after_sign_up_path_for(resource)
     if user_signed_in?
-            edit_user_path(current_user)
+            edit_user_path(resource)
+    else
+    	sign_up(resource_name, resource)
+    	edit_user_path(resource)
     end
   end
+
 end

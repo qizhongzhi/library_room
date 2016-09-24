6cc442afb7a4048a951ab9123377157516bfaf5f
LeoJY <leojy1993@gmail.com>
2016-09-24 01:22:25 -0400

    add user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+
+	before_action :find_user, only:[:show, :edit, :update, :destroy]
+
+	def index
+		@users = User.all.order("created_at DESC")
+	end
+
+	def show
+		
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if user.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @user.update(user_params)
+			redirect_to user_path(@user)
+		else
+			render 'edit'			
+		end
+	end
+
+	private
+
+		def user_params
+	      params.require(:user).permit(:email, :password, :name, :address, :telephone)
+	    end
+
+	    def find_user
+			@user = User.find(params[:id])
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+	devise_for :users
+	resources :users
+	root 'users#index'
+  end
